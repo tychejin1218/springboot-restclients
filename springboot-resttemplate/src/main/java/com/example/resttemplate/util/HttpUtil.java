e9385a21ac297cf77d9d4b0c9bed71e3844bdb99
너나들이 <tychejin@naver.com>
2024-11-11 10:06:34 +0900

    Fix: Spring RestTemplate를 활용한 HTTP 요청

--- springboot-resttemplate/src/main/java/com/example/resttemplate/util/HttpUtil.java
+++ springboot-resttemplate/src/main/java/com/example/resttemplate/util/HttpUtil.java
@@ -51,14 +51,14 @@ public class HttpUtil {
    * POST 요청을 보내고 응답을 객체로 반환
    *
    * @param targetUrl    요청을 보낼 URL
-   * @param postData     요청 본문 객체
+   * @param body         요청 본문 객체
    * @param headers      요청 헤더 정보
    * @param responseType 응답을 매핑할 클래스 타입
    * @return 응답 객체
    */
-  public <T, R> ResponseEntity<R> sendPost(String targetUrl, T postData,
+  public <T, R> ResponseEntity<R> sendPost(String targetUrl, T body,
       Map<String, String> headers, Class<R> responseType) {
-    HttpEntity<T> entity = createHttpEntity(headers, postData);
+    HttpEntity<T> entity = createHttpEntity(headers, body);
     return restTemplate.exchange(targetUrl, HttpMethod.POST, entity, responseType);
   }
 
